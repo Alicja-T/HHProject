5e4a77b7456ef4cfa3da427772afd22430ee18a6
Alicja Buchman <emunah73@gmail.com>
2018-11-23 11:20:09 -0500

    refactoring graph_visual class

--- graph_visual.py
+++ graph_visual.py
@@ -2,36 +2,61 @@ import math
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, Circle, StaticLayoutProvider
-from bokeh.palettes  import Spectral8
+from bokeh.palettes  import Spectral10
 
-node_indices = [0, 1, 2, 3, 4]
+class Graph_Visual:
+    def __init__ (self, sequence, edges):
+        self.node_indices = sequence
+        self.size = len(sequence)
+        self.edges = edges
 
-plot = figure(title="My little Graph", x_range=(-1.1, 1.1),
-y_range=(-1.1,1.1), tools="", toolbar_location=None)
+    def layout(self):
+        if self.size < 10:
+            circ = [i*2*math.pi/self.size for i in node_indices]
+            y = [math.cos(i) for i in circ]
+            x = [math.sin(i) for i in circ]
+        return zip(x,y)
+        if self.size > 10:
+            circ = [i*2*math.pi/self.size for i in node_indices]
+            y = [math.cos(i) for i in circ]
+            x = [math.sin(i) for i in circ]
 
-graph = GraphRenderer()
-graph.node_renderer.data_source.data = dict(
-    index=node_indices,
-    fill_color=Spectral8[:5]
-)
-graph.node_renderer.data_source.add(Spectral8[:5], 'color')
-graph.node_renderer.glyph = Circle(size=25, fill_color='color')
+    def create_plot(self):
+        plot = figure(title="Graph from sequence", x_range=(-1.1, 1.1),
+        y_range=(-1.1,1.1), tools="", toolbar_location=None)
 
-start_points = [0, 1, 2, 3, 4, 1, 2]
-end_points = [1, 2, 3, 4, 0, 3, 0]
-graph.edge_renderer.data_source.data = dict(
-    start = start_points,
-    end = end_points
-)
+        graph = GraphRenderer()
+        graph.node_renderer.data_source.data = dict(
+            index=self.node_indices,
+            fill_color=Spectral10 # it has to be done depending on size
+        )
+        graph.node_renderer.data_source.add(Spectral10, 'color')
+        graph.node_renderer.glyph = Circle(size=25, fill_color='color')
 
-circ = [i*2*math.pi/8 for i in node_indices]
-y = [math.cos(i) for i in circ]
-x = [math.sin(i) for i in circ]
 
-graph_layout = dict(zip(node_indices, zip(x,y)))
-graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
-plot.renderers.append(graph)
-plot.axis.visible = False
-plot.grid.visible = False
-output_file("graph.html")
-show(plot)
+        start_points, end_points = map( list, zip(*self.edges) )
+
+        graph.edge_renderer.data_source.data = dict(
+            start = start_points,
+            end = end_points
+        )
+
+        circ = [i*2*math.pi/10 for i in self.node_indices]
+        y = [math.cos(i) for i in circ]
+        x = [math.sin(i) for i in circ]
+        degrees = [3] * 10
+
+        graph_layout = dict(zip(self.node_indices, zip(x,y)))
+        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+        plot.renderers.append(graph)
+        plot.axis.visible = False
+        plot.grid.visible = False
+        plot.text(x, y, text=["%d" % i for i in degrees],
+        text_baseline="middle", text_align="center")
+        output_file("graph.html")
+        show(plot)
+
+vertices = [i for i in range(10)]
+edges = [[0, 1], [0, 2], [0, 3], [4, 5], [4, 6], [4, 7], [8, 9], [8, 1], [8, 2], [3, 5], [3, 6], [7, 9], [7, 1], [2, 5], [6, 9]]
+graph = Graph_Visual(vertices, edges)
+graph.create_plot()
